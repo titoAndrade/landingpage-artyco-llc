37b05866f324c65c93202c59b0d2fa779e38d2f9
Tito Andrade <titoraulandradevera@gmail.com>
2023-04-23 15:53:06 -0500

    feat: dev branch is live

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,9 @@
 import React, { useRef } from "react";
 import { useTranslation } from "react-i18next";
+// import ReactPlayer from 'react-player';
+// import YouTube from 'react-youtube';
+import VideoBackground from '../components/Background/VideoBackground'
+
 
 // import video from './media/video.mp4'
 
@@ -8,12 +12,15 @@ import './Home.css'
 export default function Home() {
    const { t } = useTranslation();
 
+   
+
     return(
         <div className='' id='homeContainer'>
             {/* BACKGROUND VIDEO */}
-            {/* <video autoPlay loop muted >
-                <source src={video} type='video/mp4'/>
-            </video> */}
+            <div className="video-background">
+                <VideoBackground />
+            </div>
+
 
             <div className="Home">
                 <h1>{t("hi")}</h1>
